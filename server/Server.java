7041d337da447cdd3c0b48a66bdc795838827cda
wdlzz926 <zizhuo_lin@umail.ucsb.edu>
2020-04-26 21:15:34 -0400

    buggy server

--- server/Server.java
+++ server/Server.java
@@ -9,11 +9,14 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
 
 import javax.crypto.BadPaddingException;
@@ -38,6 +41,8 @@ public class Server {
     private MessageSender messageSender = new MessageSender(1);
     private int blockchain_port;
     private List<String> candidates = new LinkedList<String>();
+    private Map<String,Integer> vote_count = new HashMap<String,Integer>();
+    private Set<String> voted_client = new HashSet<String>();
     private PrivateKey pvt;
     private PublicKey pub;
     private String user_name;
@@ -140,6 +145,7 @@ public class Server {
                     }
                     if (found) {
                         candidates.add(candidate);
+                        vote_count.put(candidate,0);
                         returnCode = 200;
                         StatusReply reply = new StatusReply(true);
                         respText = gson.toJson(reply);
@@ -208,22 +214,53 @@ public class Server {
                     byte[] tmp = Base64.getDecoder().decode(session_key);
                     byte[] key_bytes = cipher.doFinal(tmp);
                     SecretKeySpec key = new SecretKeySpec(key_bytes,"AES");
-                    cipher = cipher.getInstance();
+                    cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                    cipher.init(Cipher.DECRYPT_MODE,key);
+                    String decrypt_contents = new String(cipher.doFinal(Base64.getDecoder().decode(contents)));
+                    Encrypted_vote vote = gson.fromJson(decrypt_contents, Encrypted_vote.class);
+                    
+                    //check signature
+                    PublicKey client_public = getPulicKey(vote.getUserName());
+                    Signature sign = Signature.getInstance("SHA1withRSA");
+                    sign.initVerify(client_public);
+                    byte[] signature = Base64.getDecoder().decode(vote.getSignature());
+                    Boolean verify = sign.verify(signature);
+                    
+                    if(!verify){
+                        //wrong signature
+                        throw new Exception();
+                    }
+
+                    String user_name = vote.getUserName();
+                    if(voted_client.contains(user_name)){
+                        returnCode = 409;
+                        StatusReply reply = new StatusReply(false, "DuplicateVote");
+                        respText = gson.toJson(reply);
+                        this.generateResponseAndClose(exchange, respText, returnCode);
+                        return;
+                    }
+                    
+                    String vote_candidate = vote.getVotedFor();
+                    if(candidates.contains(vote_candidate)){
+                        int cur_vote = vote_count.get(vote_candidate);
+                        vote_count.put(vote_candidate, cur_vote+1);
+                        returnCode = 200;
+                        StatusReply reply = new StatusReply(true);
+                        respText = gson.toJson(reply);
+                    }else{
+                        returnCode = 422;
+                        StatusReply reply = new StatusReply(false, "InvalidCandidate");
+                        respText = gson.toJson(reply);
+                    }
 
 
-                } catch (NoSuchAlgorithmException e) {
-                    System.out.println("invalid algo");
-                    e.printStackTrace();
-                } catch (NoSuchPaddingException e) {
-                    e.printStackTrace();
-                } catch (InvalidKeyException e) {
-                    System.out.println("invalid key");
-                    e.printStackTrace();
-                } catch (IllegalBlockSizeException e) {
+
+
+                } catch (Exception e) {
                     // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (BadPaddingException e) {
-                    // TODO Auto-generated catch block
+                    returnCode = 422;
+                    StatusReply reply = new StatusReply(false, "WrongDuringDecryption");
+                    respText = gson.toJson(reply);
                     e.printStackTrace();
                 }
 
@@ -239,9 +276,58 @@ public class Server {
         }));
     }
 
+    private PublicKey getPulicKey(String user_name) throws Exception {
+        String uri = HOST_URI + blockchain_port + GET_CHAIN_URI;
+        GetChainRequest chain_requst = new GetChainRequest(1);
+        GetChainReply chain_reply = messageSender.post(uri, gson.toJson(chain_requst), GetChainReply.class);
+        List<Block> blocks = chain_reply.getBlocks();
+//        Boolean found = false;
+        for (Block b : blocks) {
+            if (user_name.equals(b.getData().get("user_name"))) {
+                String publickey = b.getData().get("public_key");
+                byte[] publicBytes = Base64.getDecoder().decode(publickey);
+                X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                PublicKey pubKey = keyFactory.generatePublic(keySpec);
+                return pubKey;
+            }
+        }
+        return null;
+    }
+
     private void countVote(){
         this.server_skeleton.createContext("/countvotes", (exchange -> {
+            String respText = "";
+            int returnCode = 200;
+            if ("POST".equals(exchange.getRequestMethod())) {
+                CountVotesRequest cvr;
+                try {
+                    InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+                    cvr = gson.fromJson(isr, CountVotesRequest.class);
+                } catch (Exception e) {
+                    respText = "Error during parse JSON object!\n";
+                    returnCode = 400;
+                    this.generateResponseAndClose(exchange, respText, returnCode);
+                    return;
+                }
 
+                String candidate_id = cvr.getCountVotesFor();
+                if(vote_count.containsKey(candidate_id)){
+                    int count = vote_count.get(candidate_id);
+                    CountVotesReply reply = new CountVotesReply(true, count);
+                    returnCode = 200;
+                    respText = gson.toJson(reply);
+                }else{
+                    returnCode = 422;
+                    StatusReply reply = new StatusReply(false, "InvalidCandidate");
+                    respText = gson.toJson(reply);
+                }
+                
+            }else{
+                respText = "The REST method should be POST for <service api>!\n";
+                returnCode = 400;
+            }
+            this.generateResponseAndClose(exchange, respText, returnCode);
         }));
     }
     /**
